f0fe6f631d135678ba51400976c2e77527cd379a
Matthew R. Kasun <mkasun@nusak.ca>
2022-05-14 11:29:49 -0400

    update install script
    Signed-off-by: Matthew R. Kasun <mkasun@nusak.ca>

--- Casks/netclient.rb
+++ Casks/netclient.rb
@@ -1,6 +1,6 @@
 cask "netclient" do
   version "0.14.1"
-  sha256 "d5fe8160817b3b33b5a0a4c02b88d1d8f6f47efd2127b8d5b4676c370bcd5e24"
+  sha256 "560c0946b0d60338f84d9f24369e9d98baa17966e52a86c2d35635711c51a177"
 
   url "https://github.com/gravitl/homebrew-netclient/releases/download/v0.1.0/netclient.tgz"
   name "netclient"
@@ -8,7 +8,6 @@ cask "netclient" do
   homepage "https://github.com/gravitl/netmaker"
   depends_on formula: "wireguard-tools"
   binary "netclient",  target: "netclient"
-  service "com.gravitl.netclient.plist"
 
   postflight do
     set_permissions "/Applications/netclient", '0755'
--- Casks/scripts/install.sh
+++ Casks/scripts/install.sh
@@ -1,6 +1,6 @@
 #!/usr/local/bin/bash
 
-cp /usr/local/Caskroom/netclient/0.14.1/com.gravitl.netclient.plist /Library/LaunchDaemons/
+cp /usr/local/Caskroom/netclient/VERSION/com.gravitl.netclient.plist /Library/LaunchDaemons/
 chown root /Library/LaunchDaemons/com.gravitl.netclient.plist
 launchctl load /Library/LaunchDaemons/com.gravitl.netclient.plist
 
