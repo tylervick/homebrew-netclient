c966740ddf2aca66da09ca8a1026b819cc7d23d1
Matthew R Kasun <mkasun@gmail.com>
2022-05-19 07:06:45 -0400

    Update netclient.rb

--- Casks/netclient.rb
+++ Casks/netclient.rb
@@ -3,9 +3,9 @@ cask "netclient" do
     version "0.14.0"
 
   if Hardware::CPU.intel?
-    sha256 0db17817a6ffc9faaacd424163482347c72862b77e5b3a391d486ed7bc03fe54
+    sha256 "0db17817a6ffc9faaacd424163482347c72862b77e5b3a391d486ed7bc03fe54"
   else
-    sha256 41f82d49b913e4bd58fca92f280bb0c48ee27480e9db1b19a1d16035207a02b4
+    sha256 "41f82d49b913e4bd58fca92f280bb0c48ee27480e9db1b19a1d16035207a02b4"
   end
 
   url "https://github.com/gravitl/homebrew-netclient/releases/download/v#{version}/netclient-#{arch}.tgz"
