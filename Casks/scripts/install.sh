047b8830550b55df04574200127f9dd3492e950e
Matthew R. Kasun <mkasun@nusak.ca>
2022-05-19 06:51:58 -0400

    brew --prefix
    Signed-off-by: Matthew R. Kasun <mkasun@nusak.ca>

--- Casks/scripts/install.sh
+++ Casks/scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp $(brew-prefix)/Caskroom/netclient/VERSION/com.gravitl.netclient.plist /Library/LaunchDaemons/
+cp $(brew --prefix)/Caskroom/netclient/VERSION/com.gravitl.netclient.plist /Library/LaunchDaemons/
 chown root /Library/LaunchDaemons/com.gravitl.netclient.plist
 launchctl load /Library/LaunchDaemons/com.gravitl.netclient.plist
 
