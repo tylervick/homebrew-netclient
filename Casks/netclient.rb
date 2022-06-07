ead1f778268d36733b9a348112515cadbfdeeb39
Mattthew R Kasun <mkasun@gmail.com>
2022-06-07 18:51:30 +0000

    v0.14.2

--- Casks/netclient.rb
+++ Casks/netclient.rb
@@ -1,11 +1,11 @@
 cask "netclient" do
   arch = Hardware::CPU.intel? ? "amd64" : "arm64"
-  version "0.14.2"
+  version "0.14.1"
 
   if Hardware::CPU.intel?
-    sha256 "483c696e3e12223dbabe565dbb97a13470164485a5326a43bbd4ff933557b0fa"
+    sha256 "08f2d3690307d18b93a6f873f81568c79b465560f3bed1facd81fd0e33d7dc63"
   else
-    sha256 "a312aa5187b6f5d51cd8c00ac1241e47f20aa6143c408831a50d62196f72a3e7"
+    sha256 "068ed19b6637257ba9c740704e3c7c7f6ca1b210a79403de60ea78de7c72773e"
   end
 
   url "https://fileserver.netmaker.org/v#{version}/darwin/netclient-#{arch}.tgz"
