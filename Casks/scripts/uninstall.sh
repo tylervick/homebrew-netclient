addea5d57919ac7ef6f9c5dccfc26284f29b151a
Matthew R. Kasun <mkasun@nusak.ca>
2022-09-06 09:01:20 -0400

    script updates for M1

--- Casks/scripts/uninstall.sh
+++ Casks/scripts/uninstall.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin
+export PATH=/usr/local/bin:/usr/homebrew/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin
 if [ -f /usr/local/bin/netclient ]; then
         netclient uninstall
+fi
+if [ -f /usr/homebrew/bin/netclient ]; then
+    	netclient uninstall
 fi
 if [ -f /var/log/com/gravitl.netclient.log ]; then
     rm /var/log/com.gravitl.netclient.log
