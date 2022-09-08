1c1195bfa41e4b5812db90a709b3f368fa0c9518
Matthew R. Kasun <mkasun@nusak.ca>
2022-09-08 10:33:02 -0400

    another attemp

--- Casks/scripts/install.sh
+++ Casks/scripts/install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 cp "$(brew --prefix)"/Caskroom/netclient/VERSION/com.gravitl.netclient.plist /Library/LaunchDaemons/
+chmod 0775 -c "$(brew --prefix)"/bin/netclient
+xattr -c "$(brew --prefix)"/bin/netclient
 chown root /Library/LaunchDaemons/com.gravitl.netclient.plist
 launchctl load /Library/LaunchDaemons/com.gravitl.netclient.plist
 
