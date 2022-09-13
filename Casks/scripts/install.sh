eecf84119fafe25ad01b7aad44792eef6ad1f67d
Mattthew R Kasun <mkasun@gmail.com>
2022-09-13 19:27:29 +0000

    update install script

--- Casks/scripts/install.sh
+++ Casks/scripts/install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cp "$(brew --prefix)"/Caskroom/netclient/VERSION/com.gravitl.netclient.plist /Library/LaunchDaemons/
-chown root /Library/LaunchDaemons/com.gravitl.netclient.plist
+sudo chown root /Library/LaunchDaemons/com.gravitl.netclient.plist
+sudo xttr -c "$(brew --prefix)"/bin/netclient
 launchctl unload /Library/LaunchDaemons/com.gravitl.netclient.plist
 launchctl load /Library/LaunchDaemons/com.gravitl.netclient.plist
 
